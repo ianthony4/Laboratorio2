00b7cf3c441c78c20d5e58a17a3ffceee228d000
Anthony Joaquin Ticona Hareth <aticonaha@unsa.edu.pe>
2022-05-07 14:21:32 -0500

    Variables y listener para el botón

---
+++ Ejercicio5/script.js
@@ -0,0 +1,8 @@
+let botonCrear = document.getElementById("crearTabla");
+let cantidadValores = document.getElementById("valores");
+let tabla = "";
+
+botonCrear.addEventListener("click", crearTabla);
+function crearTabla(){
+    
+}
