7c568360e38b3d071694b2d95cdfc202ec891b1b
Anthony Joaquin Ticona Hareth <aticonaha@unsa.edu.pe>
2022-05-07 14:34:37 -0500

    La cantidad de valores almacenada en el script

--- Ejercicio5/script.js
+++ Ejercicio5/script.js
@@ -1,8 +1,17 @@
 let botonCrear = document.getElementById("crearTabla");
-let cantidadValores = document.getElementById("valores");
+let cantidadValores = "";
 let tabla = "";
 
 botonCrear.addEventListener("click", crearTabla);
 function crearTabla(){
-    
+    cantidadValores = document.getElementById("valores").value;
+
+    tabla = `<table>
+    <tr>
+        <th>Valores</th>
+    </tr>`;
+    alert("Le dio al boton");
+    for(let i = 0; i < cantidadValores; i++){
+
+    }
 }
