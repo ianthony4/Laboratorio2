d0f93f7e81efd32eec4d56afd41bfde04a009141
Anthony Joaquin Ticona Hareth <aticonaha@unsa.edu.pe>
2022-05-07 15:46:00 -0500

    Renombrando

--- Ejercicio5/script.js
+++ Ejercicio5/script.js
@@ -1,7 +1,7 @@
 let botonCrear = document.getElementById("crearTabla");
 let cantidadValores = "";
 let tabla = "";
-let botonSuma = "";
+let botonSumaHtml = "";
 let arregloValores = [];
 let sumaT = 0;
 
@@ -9,30 +9,30 @@ botonCrear.addEventListener("click", crearTabla);
 
 function crearTabla(){
     cantidadValores = document.getElementById("valores").value;
-
     tabla = `<table border="1">
     <tr>
         <th>Valores</th>
     </tr>`;
-    alert("Le dio al boton");
+
     for(let i = 0; i < cantidadValores; i++){
         let numero = aleatorio();
         arregloValores.push(numero);
         tabla += "<tr><td>"+numero+"</td></tr>";
     }
-    tabla += "</table>";
-    botonSuma = `<button type="button" id="suma">Calcular suma</button>`;
-    document.getElementById("tabla").innerHTML = tabla + botonSuma;
 
-    let botonS = document.getElementById("suma");
-    botonS.addEventListener("click", sumarValores);
+    tabla += "</table>";
+    botonSumaHtml = `<button type="button" id="suma">Calcular suma</button>`;
+    document.getElementById("tabla").innerHTML = tabla + botonSumaHtml;
+
+    let botonSuma = document.getElementById("suma");
+    botonSuma.addEventListener("click", sumarValores);
 
     function sumarValores(){
         for(i = 0; i < arregloValores.length; i++){
             sumaT += arregloValores[i];
         }
-        alert(sumaT);
-}
+        alert(`La suma de todos los números es: ${sumaT}`);
+    }
 }
 
 function aleatorio(){
