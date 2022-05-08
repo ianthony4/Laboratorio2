1d830a13393ccaf5abbdd4a9a2fafd3ba94a49bc
axel almonte <aalmontecu@unsa.edu.pe>
2022-05-08 14:15:20 -0500

    se añadio un arreglo con los dias de la semana

--- ejercico_1/fecha.js
+++ ejercico_1/fecha.js
@@ -1,4 +1,5 @@
 function respuesta() {
     let dia = document.getElementById("num").value;
     console.log(dia);
+    const days = ["Domingo" , "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
 }
