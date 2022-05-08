a2219b291b641f2662642f0e4c4971f3b2930052
axel almonte <aalmontecu@unsa.edu.pe>
2022-05-08 14:20:24 -0500

    se añadio una variable que extrae el dia de la semana

--- ejercico_1/fecha.js
+++ ejercico_1/fecha.js
@@ -2,4 +2,6 @@ function respuesta() {
     let dia = document.getElementById("num").value;
     console.log(dia);
     const days = ["Domingo" , "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+    let day = days[dia];
+    console.log(day);
 }
