95b841d6f738ddb6ba38e7f46b5841c91f4bc133
Anthony Leonel Chaisa Fernandez <achaisa@unsa.edu.pe>
2022-05-09 16:55:07 -0500

    asignacion de valor a 'substr' en caso de un enlace corto, posteriormente remplazarlo en el html

--- Ejercicio4/myScript.js
+++ Ejercicio4/myScript.js
@@ -1,4 +1,8 @@
 function seleccion(){
     let str = document.getElementById(link).value;
     let substr;
+    if(str.length == 28){
+        substr = str.slice(16,19)+str.slice(20,24)+str.slice(25,28);
+        document.getElementById("respuesta").innerHTML = substr;
+    }
 }
