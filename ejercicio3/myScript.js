fdb7775e57c807a155ce25a137e0a3e17167a7f6
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2022-05-09 13:25:34 -0500

    se esat seteando el año del dia de arequipa en el if

--- ejercicio3/myScript.js
+++ ejercicio3/myScript.js
@@ -2,7 +2,9 @@ let ahora = new Date();
 let year = ahora.getFullYear();
 let diaFestivo = new Date(year, 7, 15);
 
-if (ahora.getMonth() == 7 && ahora.getDate() == 15){
+if (ahora.getMonth() == 7 && ahora.getDate() > 15){
     let year += 1;
+    diaFestivo.setFullYear(year);
 }
+
 
