40ebc39dd80ca18cdb7dae70c534a08c5f2f914c
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2022-05-09 13:35:30 -0500

    se ha corregido una doble declaracion

--- ejercicio3/myScript.js
+++ ejercicio3/myScript.js
@@ -3,7 +3,7 @@ let year = ahora.getFullYear();
 let diaFestivo = new Date(year, 7, 15);
 
 if (ahora.getMonth() == 7 && ahora.getDate() > 15){
-    let year += 1;
+    year += 1;
     diaFestivo.setFullYear(year);
 }
 
