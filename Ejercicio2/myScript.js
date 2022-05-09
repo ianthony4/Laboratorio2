460f64c96c3e198755e4b6e142e7d4162a3221cf
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-05-08 20:06:48 -0500

    Creo la funcion que invierte la palabra

--- Ejercicio2/myScript.js
+++ Ejercicio2/myScript.js
@@ -2,5 +2,13 @@
 function showAnswer(){
   let texto = document.getElementById("texto").value;
   let respuesta = document.getElementById("respuesta");
-  respuesta.innerHTML = "<h3>Prueba</h3>";
+  
+  reverseText(texto);
+}
+function reverseText(text){
+  let answer = "";
+  for (let i = 0; i <= text.length; i++) {
+    answer += text.substr(text.length - i, 1);
+  }
+  console.log(answer);
 }
