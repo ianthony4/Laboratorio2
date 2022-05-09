76909106a9aadf634ad66f0879f1a3103d9050f4
Anthony Leonel Chaisa Fernandez <achaisa@unsa.edu.pe>
2022-05-09 16:56:35 -0500

    asignacion de valor a 'substr' en caso de un enlace largo, posteriormente remplazarlo en el html

--- Ejercicio4/myScript.js
+++ Ejercicio4/myScript.js
@@ -4,5 +4,8 @@ function seleccion(){
     if(str.length == 28){
         substr = str.slice(16,19)+str.slice(20,24)+str.slice(25,28);
         document.getElementById("respuesta").innerHTML = substr;
+    }else if(str.length == 36){
+        substr = str.slice(24,27)+str.slice(28,32)+str.slice(33,36);
+        document.getElementById("respuesta").innerHTML = substr;
     }
 }
