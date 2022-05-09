8332c89dc1a215517aa7a58aa57a94e22007a064
Anthony Leonel Chaisa Fernandez <achaisa@unsa.edu.pe>
2022-05-09 17:01:04 -0500

    correccion del error, debido a que no se reconocida el ID de 'link'

--- Ejercicio4/myScript.js
+++ Ejercicio4/myScript.js
@@ -1,5 +1,5 @@
 function seleccion(){
-    let str = document.getElementById(link).value;
+    let str = document.getElementById("link").value;
     let substr;
     if(str.length == 28){
         substr = str.slice(16,19)+str.slice(20,24)+str.slice(25,28);
