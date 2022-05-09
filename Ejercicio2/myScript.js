2ca7dbd43acc48e76771b9bcb1a41579f43458c2
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-05-08 20:11:06 -0500

    Imprimo el resultado en la pagina web

--- Ejercicio2/myScript.js
+++ Ejercicio2/myScript.js
@@ -3,12 +3,13 @@ function showAnswer(){
   let texto = document.getElementById("texto").value;
   let respuesta = document.getElementById("respuesta");
   
-  reverseText(texto);
+  let palabraInvertida = reverseText(texto);
+  respuesta.innerHTML = `<h3>${palabraInvertida}</h3>`;
 }
 function reverseText(text){
   let answer = "";
   for (let i = 0; i <= text.length; i++) {
     answer += text.substr(text.length - i, 1);
   }
-  console.log(answer);
+  return answer;
 }
